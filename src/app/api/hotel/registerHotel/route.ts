b7f59300f07cc21d9da0a15f902d7d2beaa91c1f
elguapo87 <pglogovac@gmail.com>
2025-06-09 01:38:40 +0200

    Created registerHotel API route

---
+++ src/app/api/hotel/registerHotel/route.ts
@@ -0,0 +1,38 @@
+import { protectUser } from "@/middleware/userAuth";
+import hotelModel from "@/models/hotelModel";
+import userModel from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+    try {
+        const { authorized, user } = await protectUser();
+        if (!authorized || !user) {
+            return NextResponse.json({ success: false, message: "Unauthorized" }, { status: 401 });
+        }
+
+        const { name, address, contact, city } = await req.json();
+
+        const owner = user._id;
+
+        // Check if user is already registered
+        const hotel = await hotelModel.findOne({ owner });
+        if (hotel) {
+            return NextResponse.json({ success: false, message: "Hotel already registered" });
+        }
+
+        await hotelModel.create({
+            name,
+            address,
+            contact,
+            city
+        });
+
+        await userModel.findByIdAndUpdate(owner, { role: "hotelOwner" });
+
+        return NextResponse.json({ success: true, message: "Hotel registered successfully" });
+
+    } catch (error) {
+        const message = error instanceof Error ? error.message : "Unknown error";
+        return NextResponse.json({ success: false, message }, { status: 500 });
+    }
+};
