8afcde479b99e823baa1e7aeb78e031a21104990
elguapo87 <pglogovac@gmail.com>
2025-06-09 23:42:17 +0200

    Created changeAvailability API route

---
+++ src/app/api/hotel/changeAvailability/route.ts
@@ -0,0 +1,20 @@
+import roomModel from "@/models/roomModel";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+    try {
+        const { roomId } = await req.json();
+
+        const roomData = await roomModel.findById(roomId);
+
+        roomData.isAvailable = !roomData.isAvailable;
+
+        await roomData.save();
+
+        return NextResponse.json({ success: true, message: "Availability changed" });
+
+    } catch (error) {
+        const message = error instanceof Error ? error.message : "Unknown error";
+        return NextResponse.json({ success: false, message }, { status: 500 });
+    }
+};
