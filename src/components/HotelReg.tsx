41c07a1097884f73b01c0cc0521ff3faec926aeb
elguapo87 <pglogovac@gmail.com>
2025-06-11 01:47:29 +0200

    Added file input in HotelReg component

--- src/components/HotelReg.tsx
+++ src/components/HotelReg.tsx
@@ -16,6 +16,7 @@ const HotelReg = () => {
     const [contact, setContact] = useState("");
     const [address, setAddress] = useState("");
     const [city, setCity] = useState("");
+    const [image, setImage] = useState<File | null>(null);
 
     const onSubmitHandler = async (e: { preventDefault: () => void }) => {
         e.preventDefault();
@@ -62,33 +63,44 @@ const HotelReg = () => {
 
                 <div className='relative flex flex-col items-center md:w-1/2 p-8 md:p-10'>
                     <Image onClick={() => setShowHotelReg(false)} src={assets.closeIcon} alt='Close-Icon' className='absolute top-4 right-4 h-4 w-4 cursor-pointer' />
-                    <p className='text-2xl font-semibold mt-6'>Register Your Hotel</p>
+                    <p className='text-lg md:text-2xl font-semibold md:mt-6'>Register Your Hotel</p>
+
+                    {/* HOTEL IMAGE */}
+                    <div className="w-full mt-4 md:mt-6">
+                        <label htmlFor="image" className="flex items-end gap-5" >
+                            <span className="font-medium text-gray-500 md:mb-1">Hotel Image</span>
+                            <input onChange={(e) => setImage(e.target.files && e.target.files[0])} type="file" id="image" hidden />
+                            <Image src={image ? URL.createObjectURL(image) : assets.uploadArea} width={100} height={100} alt="" className="w-18 h-10 md:w-25 md:h-15 rounded" />
+                        </label>
+                    </div>
+
+                    <div className="bg-gray-300 w-full my-1.5 md:my-3 h-[1px]"></div>
 
                     {/* HOTEL NAME */}
-                    <div className='w-full mt-4'>
+                    <div className='w-full mt-2'>
                         <label htmlFor="name" className='font-medium text-gray-500'>Hotel Name</label>
-                        <input onChange={(e) => setName(e.target.value)} value={name} type="text" id='name' placeholder='Type here' className='border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light' required />
+                        <input onChange={(e) => setName(e.target.value)} value={name} type="text" id='name' placeholder='Type here' className='border border-gray-200 rounded w-full px-3 py-1.5 md:py-2.5 mt-1 outline-indigo-500 font-light' required />
                     </div>
 
                     {/* PHONE */}
                     <div className='w-full mt-4'>
                         <label htmlFor="contact" className='font-medium text-gray-500'>Phone</label>
-                        <input onChange={(e) => setContact(e.target.value)} value={contact} type="text" id='contact' placeholder='Type here' className='border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light' required />
+                        <input onChange={(e) => setContact(e.target.value)} value={contact} type="text" id='contact' placeholder='Type here' className='border border-gray-200 rounded w-full px-3 py-1.5 md:py-2.5 mt-1 outline-indigo-500 font-light' required />
                     </div>
 
                     {/* ADDRESS */}
                     <div className='w-full mt-4'>
                         <label htmlFor="address" className='font-medium text-gray-500'>Address</label>
-                        <input onChange={(e) => setAddress(e.target.value)} value={address} type="text" id='address' placeholder='Type here' className='border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light' required />
+                        <input onChange={(e) => setAddress(e.target.value)} value={address} type="text" id='address' placeholder='Type here' className='border border-gray-200 rounded w-full px-3 py-1.5 md:py-2.5 mt-1 outline-indigo-500 font-light' required />
                     </div>
 
                     {/* CITY */}
                     <div className='w-full mt-4'>
                         <label htmlFor="city" className='font-medium text-gray-500'>City</label>
-                        <input onChange={(e) => setCity(e.target.value)} value={city} type="text" id='city' placeholder='Type here' className='border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light' required />
+                        <input onChange={(e) => setCity(e.target.value)} value={city} type="text" id='city' placeholder='Type here' className='border border-gray-200 rounded w-full px-3 py-1.5 md:py-2.5 mt-1 outline-indigo-500 font-light' required />
                     </div>
 
-                    <button type="submit" className='bg-indigo-500 hover:bg-indigo-600 transition-all text-white mr-auto px-6 py-2 rounded cursor-pointer mt-6'>Register</button>
+                    <button type="submit" className='bg-indigo-500 hover:bg-indigo-600 transition-all text-white mr-auto px-4 py-1.25 md:px-6 md:py-2 rounded cursor-pointer mt-6'>Register</button>
                 </div>
             </form>
         </div>
