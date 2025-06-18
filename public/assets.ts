71dd13ff163fcc026484cc1924a14ba176a2dc26
elguapo87 <pglogovac@gmail.com>
2025-06-18 14:19:05 +0200

    Fixed problem in assets file

--- public/assets.ts
+++ public/assets.ts
@@ -102,10 +102,32 @@ export const exclusiveOffers = [
 
 // Testimonials Dummy Data
 export const testimonials = [
-    { id: 1, name: "Emma Rodriguez", address: "Barcelona, Spain", image: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=200", rating: 5, review: "I've used many booking platforms before, but none compare to the personalized experience and attention to detail that Easy Booking provides." },
-    { id: 2, name: "Liam Johnson", address: "New York, USA", image: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=200", rating: 4, review: "Easy Booking exceeded my expectations. The booking process was seamless, and the hotels were absolutely top-notch. Highly recommended!" },
-    { id: 3, name: "Sophia Lee", address: "Seoul, South Korea", image: "https://images.unsplash.com/photo-1701615004837-40d8573b6652?q=80&w=200", rating: 5, review: "Amazing service! I always find the best luxury accommodations through Easy Booking. Their recommendations never disappoint!" }
+  {
+    id: 1,
+    name: "Emma Rodriguez",
+    address: "Barcelona, Spain",
+    image: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=200",
+    rating: 5,
+    review: "&ldquo;I've used many booking platforms before, but none compare to the personalized experience and attention to detail that Easy Booking provides.&rdquo;"
+  },
+  {
+    id: 2,
+    name: "Liam Johnson",
+    address: "New York, USA",
+    image: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=200",
+    rating: 4,
+    review: "&ldquo;Easy Booking exceeded my expectations. The booking process was seamless, and the hotels were absolutely top-notch. Highly recommended!&rdquo;"
+  },
+  {
+    id: 3,
+    name: "Sophia Lee",
+    address: "Seoul, South Korea",
+    image: "https://images.unsplash.com/photo-1701615004837-40d8573b6652?q=80&w=200",
+    rating: 5,
+    review: "&ldquo;Amazing service! I always find the best luxury accommodations through Easy Booking. Their recommendations never disappoint!&rdquo;"
+  }
 ];
+
 
 // Facility Icon
 export const facilityIcons = {
